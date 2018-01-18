da463e445b46b966449bbe2b52aa2eac72ae3717
tak <tak@linux.pl>
2018-01-18 22:31:06 +0100

    dodano ustaw()

--- dziwne/dziwne.cpp
+++ dziwne/dziwne.cpp
@@ -26,9 +26,34 @@ return Data_Wrap_Struct(obj,NULL,t_free,m);
 
 extern "C" VALUE t_cosik()
 {
+/* what I can return "żółte" string */
  VALUE str = rb_str_new2( "Dziwne" );
 
 return str;
+}
+
+/* How check the type of i is "Dziwne" or not */
+extern "C" VALUE t_ustaw(VALUE obj, VALUE i)
+{
+struct moje *m;
+
+ switch (TYPE(i))
+  {
+  case T_FIXNUM: /* set and return */
+   Data_Get_Struct(obj, struct moje, m);
+   m->x = (int)NUM2INT(i);
+   return INT2NUM(m->x);
+   break;
+  case T_NIL: /* only return */
+   Data_Get_Struct(obj, struct moje, m);
+   return INT2NUM(m->x);
+   break;
+  default:
+   rb_raise(rb_eStandardError, "Nie działa");
+   break;
+  }
+
+return Qnil;
 }
 
 
@@ -40,4 +65,5 @@ extern "C" void Init_Dziwne()
  cDziwne = rb_define_class("Dziwne", rb_cObject);
  rb_define_alloc_func(cDziwne,t_allocate);
  rb_define_method(cDziwne, "cosik", (ruby_method*) &t_cosik, 0);
+ rb_define_method(cDziwne, "ustaw", (ruby_method*) &t_ustaw, 1);
 }
--- test.rb
+++ test.rb
@@ -2,5 +2,16 @@
 # encoding: utf-8
 require_relative './dziwne/Dziwne'
 
+class Dziwne
+attr_reader :a
+
+def initialize
+ @a = 7
+end
+
+end
+
 t = Dziwne.new
+p t.ustaw(nil)
+p t.ustaw(t.a)
 p t.cosik
