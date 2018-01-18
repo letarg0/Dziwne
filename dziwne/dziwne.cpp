16d42ac086346fbc48f8b29f9f26187bde79d7b4
tak <tak@linux.pl>
2018-01-18 22:37:59 +0100

    komentarze to pytania */

--- dziwne/dziwne.cpp
+++ dziwne/dziwne.cpp
@@ -26,28 +26,34 @@ return Data_Wrap_Struct(obj,NULL,t_free,m);
 
 extern "C" VALUE t_cosik()
 {
-/* what I can return "żółte" string */
  VALUE str = rb_str_new2( "Dziwne" );
+/* VALUE str = rb_str_new2( "Żółty" );
+what I can return "żółte" string */
 
 return str;
 }
 
-/* How check the type of i is "Dziwne" or not */
+/* How check the type of 'i' is "Dziwne" or not */
 extern "C" VALUE t_ustaw(VALUE obj, VALUE i)
 {
 struct moje *m;
 
  switch (TYPE(i))
   {
-  case T_FIXNUM: /* set and return */
+  case T_FIXNUM: /* set variable and return it */
    Data_Get_Struct(obj, struct moje, m);
    m->x = (int)NUM2INT(i);
    return INT2NUM(m->x);
    break;
-  case T_NIL: /* only return */
+  case T_NIL: /* only return x */
    Data_Get_Struct(obj, struct moje, m);
    return INT2NUM(m->x);
    break;
+  /* case T_DZIWNE:
+   Data_Get_Struct(obj, struct moje, m);
+   VALUE a = rb_ary_new();
+   return rb_ary_push(a, <new empty object class "Dziwne"> );
+   break; */
   default:
    rb_raise(rb_eStandardError, "Nie działa");
    break;
@@ -58,12 +64,14 @@ return Qnil;
 
 
 
+
 VALUE cDziwne;
 
 extern "C" void Init_Dziwne()
 {
  cDziwne = rb_define_class("Dziwne", rb_cObject);
  rb_define_alloc_func(cDziwne,t_allocate);
+/* rb_define_method(cDziwne, "żółte", (ruby_method*) &t_cosik, 0); */
  rb_define_method(cDziwne, "cosik", (ruby_method*) &t_cosik, 0);
  rb_define_method(cDziwne, "ustaw", (ruby_method*) &t_ustaw, 1);
 }
